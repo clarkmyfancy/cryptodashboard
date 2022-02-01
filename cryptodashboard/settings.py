e6a787569d4fed49441a4c64fcb664da95eb56f4
Jack Conrad Clark <clark.jack0@gmail.com>
2022-02-01 11:06:15 -0600

    update secrets

--- cryptodashboard/settings.py
+++ cryptodashboard/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rcjo-bf)zaps_201ie*m5^)g2q4doulf7_duz+)txu241x_)^'
+# SECRET_KEY = 'django-insecure-8rcjo-bf)zaps_201ie*m5^)g2q4doulf7_duz+)txu241x_)^'
+SECRET_KEY = os.environ.get('CRYPTOWEBAPIDASHBOARD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
