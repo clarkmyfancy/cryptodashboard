d8a567f71643fc10922ec9a174253c4b126d5e1f
Jack Conrad Clark <clark.jack0@gmail.com>
2022-02-13 14:48:18 -0600

    add watchlist app

--- cryptodashboard/settings.py
+++ cryptodashboard/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'watchlist.apps.WatchlistConfig',
     'cryptos.apps.CryptosConfig',
     'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
