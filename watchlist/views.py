8a8c12cffe2a86b64ef072420fa6d513cedb6146
Jack Conrad Clark <clark.jack0@gmail.com>
2022-02-13 15:53:32 -0600

    switch over to use watchlist index file

--- watchlist/views.py
+++ watchlist/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from .models import Favorite
 
 class IndexView(ListView):
-    template_name = 'cryptos/index.html'
+    template_name = 'watchlist/index.html'
     context_object_name = 'cryptos'
 
     def get_queryset(self):
